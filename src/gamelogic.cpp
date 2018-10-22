afe28047d37e833d5d9964267643133c8f9888c9
zhs007 <sssxueren@gmail.com>
2018-10-22 18:01:18 +0800

    upd

--- src/gamelogic.cpp
+++ src/gamelogic.cpp
@@ -9,15 +9,19 @@ GameLogic::GameLogic() {}
 
 GameLogic::~GameLogic() {}
 
-bool GameLogic::init() {}
+bool GameLogic::init() { return true; }
 
-bool GameLogic::gameCtrl(const ::natashapb::UserGameLogicInfo* pUser) {}
+bool GameLogic::gameCtrl(const ::natashapb::UserGameLogicInfo* pUser) {
+  return true;
+}
 
 bool GameLogic::addGameMod(::natashapb::GAMEMODTYPE gmt, GameMod* pMod) {
   assert(gmt != ::natashapb::NULL_MOD);
   assert(m_mapGameMod.find(gmt) != m_mapGameMod.end());
 
   m_mapGameMod[gmt] = pMod;
+
+  return true;
 }
 
 }  // namespace natasha
